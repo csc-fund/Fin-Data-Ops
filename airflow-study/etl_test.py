6faae5f30033149ea871976d1bf548477ccd4649
colin <colin_bao@163.com>
2022-09-07 15:15:21 +0800

    Update

--- airflow-study/etl_test.py
+++ airflow-study/etl_test.py
@@ -1,17 +1,33 @@
-import datetime
 import pendulum
-import os
-import json
-from concurrent.futures import *
-import pendulum
+from airflow.decorators import dag, task
+# -----------------airflow数据库接口----------------- #
+from airflow.providers.mysql.hooks.mysql import MySqlHook  # MySql数据库
+from airflow.providers.microsoft.mssql.hooks.mssql import MsSqlHook  # MsSql数据库
+from concurrent.futures import *  # 多进程并行
 import pandas as pd
 import numpy as np
-from airflow.decorators import dag, task
-from airflow.providers.mysql.operators.mysql import MySqlOperator  # 数据库操作
-from airflow.providers.mysql.hooks.mysql import MySqlHook
-from airflow.providers.microsoft.mssql.hooks.mssql import MsSqlHook
 from datetime import datetime, timedelta
 import logging
+
+# 配置本地日志信息
+logging.basicConfig(level=logging.DEBUG,  # 控制台打印的日志级别
+                    filename='new.log',
+                    filemode='a',  # 模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志, a是追加模式，默认如果不写的话，就是追加模式
+                    format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s'  # 日志格式
+                    )
+
+
+# 定义失败提醒,任意一个任务失败整个DAG失败
+def task_failure_alert(context):
+    from msg.MailMsg import MailMsg
+    with MailMsg() as msg:
+        msg.send_msg('任务失败',
+                     f"Task has failed, task_instance_key_str: {context['task_instance_key_str']}")
+
+
+# 定义成功提醒 放在任务流的末尾,所有任务流成功即DAG成功
+def dag_success_alert(context):
+    print(f"DAG has succeeded, run_id: {context['run_id']}")
 
 
 # [START DAG] 实例化一个DAG
@@ -23,7 +39,7 @@ import logging
 )
 # 在DAG中定义任务
 def taskflow_api_etl():
-    @task  # 提取-> 从数据库按照日期提取需要的表
+    @task(on_failure_callback=task_failure_alert)  # 提取-> 从数据库按照日期提取需要的表
     def extract_sql(table_name, conn_id='airflow_db') -> pd.DataFrame:
         # ----------------- 从Airflow保存的connection获取多数据源连接----------------- #
         sql_hook = MySqlHook(mysql_conn_id=conn_id)
@@ -33,47 +49,27 @@ def taskflow_api_etl():
         df_extract = sql_hook.get_pandas_df(query_sql)  # 从Airflow的接口返回df格式
         return df_extract  # 传递到下一个子任务
 
-    @task  # 变换 -> 对DataFrame进行格式转换
+    @task(on_failure_callback=task_failure_alert)  # 变换 -> 对DataFrame进行格式转换
     def transform_df(df_extract: pd.DataFrame):
         df_transform = df_extract.fillna(0)  # 数据转换
         return df_transform  # 传递到下一个子任务
 
-    @task  # 加载-> 下载feather格式到文件系统
+    @task(on_success_callback=dag_success_alert)  # 加载-> 下载feather格式到文件系统
     def load_feather(df_transform: pd.DataFrame):
-        df_transform.to_feather('load.f')  # 储存文件
         df_transform.to_csv('load.csv')  # 储存文件
 
     @task  # 通信-> 异常告警
     def send_msg(sub: str = '无主题', content: str = '无内容'):
-        print('异常告警', sub, content)
-        # return True
+        from msg.MailMsg import MailMsg
+        with MailMsg() as msg:
+            msg.send_msg(sub, content)
 
     # [START main_flow]  API 2.0 会根据任务流调用自动生成依赖项,不需要定义依赖
-    # 配置本地日志信息
-    logging.basicConfig(level=logging.DEBUG,  # 控制台打印的日志级别
-                        filename='new.log',
-                        filemode='a',  # 模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志, a是追加模式，默认如果不写的话，就是追加模式
-                        format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s'  # 日志格式
-                        )
+    append_tables = ['ab_user', 'error_test', 'error_test']  # 待完成的任务:表名
+    with ThreadPoolExecutor(max_workers=3) as executor:  # 多进程异步执行
+        _ = {executor.submit(lambda x: load_feather(transform_df(extract_sql(x))),
+                             table): table for table in append_tables}  # 每个线程的执行结果,submit()中填写函数和形参
 
-    # 多进程异步执行
-    append_tables = ['ab_user', 'ab_user', 'ab_user', 'ab_user']  # 需要下载的表名
-    with ThreadPoolExecutor(max_workers=3) as executor:
-        tasks = {executor.submit(load_feather, transform_df(extract_sql(table))): table for table in
-                 append_tables}  # 返回每个线程的执行结果,submit()中填写函数和形参,让所有进程异步执行任务
-        for future in as_completed(tasks):  # 完成select_table以后,遍历每个进程的执行情况
-            table = tasks[future]  # 每个进程对应的的表名
-            # 获取进程执行的结果
-            try:
-                res = future.result()
-            # 结果异常处理
-            except Exception as exc:
-                logging.error('%s generated an exc: %s' % (table, exc))
-                send_msg(table, str(exc))
-            # 结果正常处理
-            else:
-                logging.info('%s table res %s ' % (table, res))
-                send_msg(table, str(res))
     # [END main_flow]
 
 
@@ -82,6 +78,7 @@ def taskflow_api_etl():
 
 # [START dag_invocation]
 dag = taskflow_api_etl()
+# on_success_callback
 # [END dag_invocation]
 
 
--- airflow-study/progress_test.py
+++ airflow-study/progress_test.py
@@ -4,8 +4,6 @@
 # @Time      :2022/9/7 08:11
 # @Author    :Colin
 # @Note      :None
-
-
 from concurrent.futures import *
 import urllib.request
 
@@ -13,41 +11,60 @@ URLS = ['http://www.foxnews.com/',
         'http://www.cnn.com/',
         'http://europe.wsj.com/',
         'http://www.bbc.co.uk/',
-        'http://some-made-up-domain.com/']
+        'http://some-made-up-domain.com/',
+        'load_2',
+        'load_1']
 
 
 # Retrieve a single page and report the URL and contents
-def load_url(url):
+def load_1(url):
     # with urllib.request.urlopen(url, timeout=timeout) as conn:
     #     cont = conn.read()
     # print(url[:10])
     # if url[:10] == 'http://som':
-    #     raise Exception
-    # return url
-    print()
+    #     raise Exception\
+    # print('load1', func)
+    # print(url)
+    if url == 'load_1':
+        raise Exception
+    return url
 
 
 def load_2(url):
-    print(url)
+    if url == 'load_2':
+        raise Exception
+    return url
 
     # def __call__():
     #     return None
 
 
+def load_3(url):
+    if url == 'http://some-made-up-domain.com/':
+        raise Exception
+    # return url
+
+
 # We can use a with statement to ensure threads are cleaned up promptly
-with ThreadPoolExecutor(max_workers=5) as executor:
+with ThreadPoolExecutor(max_workers=2) as executor:
     # Start the load operations and mark each future with its URL
-    future_to_url = {executor.submit(load_2, load_url(url)): url for url in URLS}
-    print(future_to_url)
+    def muti_task(url):
+        load_3(load_2(load_1(url)))
+
+
+    future_to_url = {executor.submit(muti_task, url): url for url in URLS}
+    # future_to_url_2 = {executor.submit(load_2, url): url for url in future_to_url}
+    # future_to_url_2 = {executor.submit(load_1, url): url for url in URLS}
+    # print(future_to_url)
     for future in as_completed(future_to_url):
         url = future_to_url[future]  # 执行的表名
-        es = future.result()  # 线程执行的结果 ,没有返回就是None
-        # try:
-        #     res = future.result()  # 线程执行的结果 ,没有返回就是None
-        #     print(future)
-        # except Exception as exc:
-        #     # 异常执行
-        #     print('%r generated an exception: %s' % (url, exc))
-        # else:
-        #     # 正常执行
-        #     print('%r ok' % url)
+        # res = future.result()
+        try:
+            res = future.result()  # 线程执行的结果 ,没有返回就是None
+            # print(future)
+        except Exception as exc:
+            # 异常执行
+            print('%r generated an exception: %s' % (url, exc))
+        else:
+            # 正常执行
+            print('%r ok' % url)
